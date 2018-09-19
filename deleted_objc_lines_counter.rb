c531bd200ed8098f99de3b947ecddfd614555fed
Tatsuya Kojima <cy11121@shibaura-it.ac.jp>
2018-09-19 23:48:29 +0900

    Get pull requests

---
+++ deleted_objc_lines_counter.rb
@@ -0,0 +1,28 @@
+require 'octokit'
+
+GITHUB_API_TOKEN=ENV['GITHUB_API_TOKEN']
+GITHUB_REPO='komaji/test_danger'
+GITHUB_MILESTONE='v1.0.0'
+
+def client
+  Octokit::Client.new(access_token: GITHUB_API_TOKEN)
+end
+
+def milestone_number
+  milestones = client.list_milestones(GITHUB_REPO).find do |milestone|
+    milestone.title == GITHUB_MILESTONE
+  end
+  milestones.number
+end
+
+def user_name
+  client.user.login
+end
+
+def pull_requests
+  client.pull_requests(GITHUB_REPO, state: 'all').select do |pr|
+    pr.milestone.title == GITHUB_MILESTONE && pr.user.login == user_name
+  end
+end
+
+puts pull_requests
