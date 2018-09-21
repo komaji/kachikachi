240f4c2455b3bb1263e3e8635ed0e039d6757456
Tatsuya Kojima <cy11121@shibaura-it.ac.jp>
2018-09-21 13:34:19 +0900

    Create Patch Class

--- deleted_objc_lines_counter.rb
+++ deleted_objc_lines_counter.rb
@@ -4,6 +4,15 @@ GITHUB_API_TOKEN=ENV['GITHUB_API_TOKEN']
 GITHUB_REPO='komaji/test_danger'
 GITHUB_MILESTONE='v1.0.0'
 
+class Patch
+  attr_accessor :file_name, :body
+
+  def initialize(file_name, body)
+    @file_name = file_name || ''
+    @body = body || ''
+  end
+end
+
 def client
   Octokit::Client.new(access_token: GITHUB_API_TOKEN, auto_paginate: true)
 end
@@ -30,27 +39,30 @@ def pull_request_diff(number)
 end
 
 def pull_request_patch_list(pr_diff)
-  patch = ''
+  patch_body = ''
   patch_list = []
+  patch_file_name = ''
   body = false
   lines = pr_diff.lines
 
   lines.each_with_index do |line, index|
     case line
-    when /^diff/
-      next if patch.empty?
+    when /^diff\s--git\sa\/(?<file_name>.*)\sb\//
+      unless patch_body.empty?
+        patch_list << Patch.new(patch_file_name, patch_body)
+        patch_body = ''
+      end
 
-      patch_list << patch
-      patch = ''
+      patch_file_name = Regexp.last_match[:file_name]
       body = false
     when /^@@\s-\d+,\d+\s\+\d+,\d+\s@@/
       body = true
     else
       next unless body
 
-      patch << line
+      patch_body << line
       last_line = lines.count == index + 1
-      patch_list << patch if last_line && !patch.empty?
+      patch_list << Patch.new(patch_file_name, patch_body) if last_line && !patch_body.empty?
     end
   end
 
@@ -81,4 +93,4 @@ def only_removed_patch(pr_patch)
   remove_added_lines(remove_unmodified_lines(remove_white_space(pr_patch)))
 end
 
-puts only_removed_patch(all_pull_requests_patch_list.join("\n")).lines.count
+puts "👋👋👋 #{only_removed_patch(all_pull_requests_patch_list.map(&:body).join("\n")).lines.count} lines 👋👋👋"
