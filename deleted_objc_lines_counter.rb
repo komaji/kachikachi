39f08da23190857b22185c7ffff493f6f77f6a75
Tatsuya Kojima <cy11121@shibaura-it.ac.jp>
2018-09-24 02:18:33 +0900

    Give cli

--- deleted_objc_lines_counter.rb
+++ deleted_objc_lines_counter.rb
@@ -1,24 +1,57 @@
 require 'octokit'
+require 'thor'
 
-GITHUB_API_TOKEN=ENV['GITHUB_API_TOKEN']
-GITHUB_REPO='komaji/test_danger'
-GITHUB_MILESTONE='v1.0.0'
-TARGET_FILE_REGEXP=/^.*\.(m|h)/
-TARGET_PULL_REQUEST_NUMBERS=nil
-PULL_REQUEST_STATE='closed'
-IGNORE_WHITE_SPACE=false
+class CLI < Thor
+  desc 'count', 'Count removed code lines.'
+  option 'token', default: ENV['GITHUB_API_TOKEN']
+  option 'repo', required: true
+  option 'file-regexp'
+  option 'milestone'
+  option 'pull-request-numbers', type: :array
+  option 'state', default: :closed
+  option 'ignore-white-space', type: :boolean, default: true
+  def count
+    options[:milestone] || options['pull-request-numbers'] or raise 'Need to specify milestone or pull request numbers. Please use --milestone or --pull-request-numbers options.'
+    Counter.new(self).count
+  end
+end
+
+class Counter
+  def initialize(cli)
+    @cli = cli
+  end
+
+  def count
+    all_target_patch_list = GitHub.new(@cli.options).pull_requests
+                              .map(&:target_patch_list)
+                              .flatten
+    removed_patches_total_count = all_target_patch_list
+                                    .map(&:body)
+                                    .map(&:only_removed)
+                                    .map(&:body)
+                                    .join
+                                    .lines
+                                    .count
+
+    puts "👋👋👋 #{removed_patches_total_count} lines 👋👋👋"
+  end
+end
 
 class GitHub
   attr_accessor :client
 
+  def initialize(options)
+    @options = options
+  end
+
   def pull_requests
-    prs = client.pull_requests(GITHUB_REPO, state: PULL_REQUEST_STATE).select do |pr|
-      (!GITHUB_MILESTONE || pr.milestone&.title == GITHUB_MILESTONE) &&
-        (!TARGET_PULL_REQUEST_NUMBERS || TARGET_PULL_REQUEST_NUMBERS.include?(pr.number)) &&
+    prs = client.pull_requests(@options[:repo], state: @options[:state]).select do |pr|
+      (!@options[:milestone] || pr.milestone&.title == @options[:milestone]) &&
+        (!@options['pull-request-numbers'] || @options['pull-request-numbers'].map(&:to_i).include?(pr.number)) &&
         pr.user.login == user_name
     end
 
-    prs.map{ |pr| PullRequest.new(pr) }
+    prs.map{ |pr| PullRequest.new(pr, @options) }
   end
 
   def user_name
@@ -27,20 +60,21 @@ class GitHub
 
   private
   def client
-    @client ||= Octokit::Client.new(access_token: GITHUB_API_TOKEN, auto_paginate: true)
+    @client ||= Octokit::Client.new(access_token: @options[:token], auto_paginate: true)
   end
 end
 
 class PullRequest
   attr_accessor :content, :client
 
-  def initialize(content)
+  def initialize(content, options)
     @content = content
+    @options = options
   end
 
   def target_patch_list
     patch_list.select do |patch|
-      !TARGET_FILE_REGEXP || patch.file_name =~ TARGET_FILE_REGEXP
+      !@options['file-regexp'] || patch.file_name =~ Regexp.new(@options['file-regexp'])
     end
   end
 
@@ -55,7 +89,7 @@ class PullRequest
       case line
       when /^diff\s--git\sa\/(?<file_name>.*)\sb\//
         unless patch_body.empty?
-          patch_list << Patch.new(patch_file_name, patch_body)
+          patch_list << Patch.new(patch_file_name, patch_body, @options)
           patch_body = ''
         end
 
@@ -68,7 +102,7 @@ class PullRequest
 
         patch_body << line
         last_line = lines.count == index + 1
-        patch_list << Patch.new(patch_file_name, patch_body) if last_line && !patch_body.empty?
+        patch_list << Patch.new(patch_file_name, patch_body, @options) if last_line && !patch_body.empty?
       end
     end
 
@@ -76,7 +110,7 @@ class PullRequest
   end
 
   def diff
-    client.pull_request(GITHUB_REPO, number, accept: 'application/vnd.github.v3.diff')
+    client.pull_request(@options[:repo], number, accept: 'application/vnd.github.v3.diff')
   end
 
   def number
@@ -85,56 +119,46 @@ class PullRequest
 
   private
   def client
-    @client ||= Octokit::Client.new(access_token: GITHUB_API_TOKEN, auto_paginate: true)
+    @client ||= Octokit::Client.new(access_token: @options['token'], auto_paginate: true)
   end
 end
 
 class Patch
   attr_accessor :file_name, :body
 
-  def initialize(file_name, body)
+  def initialize(file_name, body, options)
     @file_name = file_name || ''
-    @body = PatchBody.new(body)
+    @body = PatchBody.new(body, options)
   end
 end
 
 class PatchBody
   attr_accessor :body
 
-  def initialize(body)
+  def initialize(body, options)
     @body = body || ''
+    @options = options
   end
 
   def only_removed
     patch = ignore_unmodified_lines
       .ignore_added_lines
-    patch = patch.ignore_white_space if IGNORE_WHITE_SPACE
+    patch = patch.ignore_white_space if @options['ignore-white-space']
 
     patch
   end
 
   def ignore_unmodified_lines
-    PatchBody.new(@body.gsub(/^\s.*(\n|\r\n|\r)/, ''))
+    PatchBody.new(@body.gsub(/^\s.*(\n|\r\n|\r)/, ''), @options)
   end
 
   def ignore_added_lines
-    PatchBody.new(@body.gsub(/^\+.*(\n|\r\n|\r)/, ''))
+    PatchBody.new(@body.gsub(/^\+.*(\n|\r\n|\r)/, ''), @options)
   end
 
   def ignore_white_space
-    PatchBody.new(@body.gsub(/^(-|\+)\s*(\n|\r\n|\r)/, ''))
+    PatchBody.new(@body.gsub(/^(-|\+)\s*(\n|\r\n|\r)/, ''), @options)
   end
 end
 
-all_target_patch_list = GitHub.new().pull_requests
-                      .map(&:target_patch_list)
-                      .flatten
-removed_patches_total_count = all_target_patch_list
-                                .map(&:body)
-                                .map(&:only_removed)
-                                .map(&:body)
-                                .join
-                                .lines
-                                .count
-
-puts "👋👋👋 #{removed_patches_total_count} lines 👋👋👋"
+CLI.start(ARGV)
